528cd07fba6a6eb468e4eb824f5ae96fed3a53a4
AZP7 <aungazpzawphyo@gmail.com>
2025-04-13 18:00:58 +0630

    add the repo name in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/front_end_mentor_chall_5/', // 👈 this MUST match your GitHub repo name
+
+  
 })
